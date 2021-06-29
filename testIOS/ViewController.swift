e1272e10020c86428ed96728241f3944a43e9a3f
MIrsyadRafiSudirjo <irsyadicad1302@gmail.com>
2021-06-29 15:41:21 +0700

    Initial Commit

---
+++ testIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testIOS
+//
+//  Created by Muhammad Irsyad Rafi on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
